11c93c53765cd4e63f79bb632498f63de9dd2577
NguyenMinhQuang <nmquang21@gmail.com>
2019-05-20 14:40:22 +0700

    up_demo

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+var bodyParser = require('body-parser');
+const app = express();
+const port = 9999;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/demo",function (req, res) {
+    res.send("OK!")
+});
+app.post("/register",function (req, res) {
+    var fullname = req.body.fullname;
+    var username = req.body.username;
+    var password = req.body.password;
+    res.send("INFOR: Fullname: "+fullname+" username: "+username+" Password: "+password);
+});
+app.listen(port, function () {
+    console.log("Hello World");
+})
